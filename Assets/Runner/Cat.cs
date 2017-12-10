3c545ecf18db3c49a0c84a7f7483bbc5d57edb48
Isaac Campos <kakoo26i@gmail.com>
2017-12-10 10:32:27 -0600

    Se corrige la perspectiva de la cámara

--- Assets/Runner/Cat.cs
+++ Assets/Runner/Cat.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class Cat : MonoBehaviour {
 	public enum Trail { First, Second, Third };
+	public enum Colors { Red, Green, Blue, Normal};
+	public Material[] colors;
 	public Trail trail;
+	public Colors color;
+
+	private Material material;
+
 
 	// Use this for initialization
 	void Start () {
-		Trail trail;
 		trail = Trail.First;
+		material = GetComponent<Renderer>().material;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		updateTrail();
+		UpdateTrail();
+		ChangeColor();
 	}
 
-	private Trail updateTrail() {
+	void ChangeColor() { //r = 0, g = 2, b =3
+		if (color == Colors.Red)
+		{
+			material = colors[0];
+		}
+		else if (color == Colors.Green)
+		{
+			material = colors[1];
+		}
+		else if (color == Colors.Blue)
+		{
+			material = colors[2];
+		}
+		else {
+			material = colors[3];
+		}
+	}
+
+	private Trail UpdateTrail() {
 		if (Input.GetKeyDown(KeyCode.UpArrow))
 		{
 			UpTrail();
